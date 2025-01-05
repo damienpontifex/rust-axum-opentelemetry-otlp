5ac45ed09c14600c3398e10d0069c38f2b256990
Damien Pontifex <damien.pontifex@gmail.com>
2025-01-05 19:23:04 +0800

    Dotnet project to test trace context propagation

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ docker run --rm -d --name jaeger \
 ```
 
 n.b. not meant to be a reference or replacement for other libraries, but always had difficulties setting these up so a little experiment/reminder for myself
+
+- `dotnet-project/`: A dotnet web api project to test and validate the trace context propagation is working as expected
+    - Running both together and hitting http://localhost:5000/weatherforecast will then hit the rust endpoints and show both the dotnet and rust api project trace tree within jaeger
---
+++ dotnet-project/Program.cs
@@ -0,0 +1,47 @@
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+var otel = builder.Services.AddOpenTelemetry();
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export via OTLP
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+    tracing.SetSampler(new AlwaysOnSampler());
+});
+otel.UseOtlpExporter();
+
+var app = builder.Build();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", async (IHttpClientFactory httpClientFactory) =>
+{
+    using var client = httpClientFactory.CreateClient();
+    var res = await client.GetAsync("http://localhost:3000/dotnet");
+
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
